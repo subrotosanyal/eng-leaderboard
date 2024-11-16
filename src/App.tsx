a5f776c9cb3d456b3bbc076a6edcb0d23d546b19
Subroto Sanyal <sanyalsubroto@gmail.com>
2024-11-16 11:57:26 +0100

    Added support for dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -168,7 +168,7 @@ function App() {
                             </div>
                         ) : (
                             <>
-                                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6 mb-8">
+                                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
                                     {filteredDevelopers.map((developer, index) => (
                                         <LeaderCard
                                             key={developer.id}
